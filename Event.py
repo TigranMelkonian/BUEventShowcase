9a5554fc6af80b07d1142098f1d3c85824b70692
aquan6 <aquan@bu.edu>
2017-10-31 19:37:26 -0400

    added JSON file read/writing

--- Event.py
+++ Event.py
@@ -3,8 +3,10 @@
 Event Object Sketch for PCT Project
 """
 import datetime
+import json
 
 class Event:
+    data = {}
     organizer = ""
     participants = ""
     description = ""
@@ -38,8 +40,9 @@ class Event:
     
     #Don't know how these will be used yet
     ID = 123
-    otherID = "asdf"
+    otherIDs = "asdf"
     
+    '''Constructor with all the required fields needed'''
     def __init__(self, organ, parti, desc, regReq, loc, inCost, inDate, endDate):
         self.organizer = organ
         self.participants = parti
@@ -49,7 +52,69 @@ class Event:
         self.cost = inCost
         self.startTime = inDate
         self.endTime=endDate
+    
+    
+    '''Converts existing data in the object into a dictionary, then returns it as a JSON object'''
+    def getJSON(self):
+        self.data = {"organizer":self.organizer, "participants":self.participants, "description":self.description, \
+                     "tags": self.tags, "registrationRequired":self.registrationRequired, "location":self.location, \
+                     "address":self.address, "city":self.city, "zipCode":self.zipCode, "startTime":self.startTime, \
+                     "endTime": self.endTime, "duration":self.duration, "cost":self.cost, "minCost":self.minCost, \
+                     "maxCost":self.maxCost, "refundPolicy":self.refundPolicy, "subOrganizers":self.subOrganizers, \
+                     "sponsors": self.sponsors, "ID":self.ID}
+        json_data = json.dumps(self.data)
+        return json_data
+    
+    '''Reads in JSON file and saves it into the object'''
+    def jsonToEventObject(self,fileName):
+        with open(fileName) as data_file:    
+            self.data = json.load(data_file)
         
+        for key,value in self.data.items():
+            #self.eval(key)=value
+            #yeah i know this is bad way to reference things
+            if(key=="organizer"):
+                self.organizer=value
+            elif(key=="participants"):
+                self.participants=value
+            elif(key=="description"):
+                self.description=value
+            elif(key=="tags"):
+                self.tags=value
+            elif(key=="registrationRequired"):
+                self.registrationRequired=value
+            elif(key=="location"):
+                self.location=value
+            elif(key=="address"):
+                self.address=value
+            elif(key=="city"):
+                self.city=value
+            elif(key=="zipCode"):
+                self.zipCode=value
+            elif(key=="startTime"):
+                self.startTime=value
+            elif(key=="endTime"):
+                self.endTime=value
+            elif(key=="duration"):
+                self.duration=value
+            elif(key=="cost"):
+                self.cost=value
+            elif(key=="minCost"):
+                self.minCost=value
+            elif(key=="maxCost"):
+                self.maxCost=value
+            elif(key=="refundPolicy"):
+                self.refundPolicy=value
+            elif(key=="subOrganizers"):
+                self.subOrganizers=value
+            elif(key=="sponsors"):
+                self.sponsors=value
+            elif(key=="ID"):
+                self.ID=value
+        self.getJSON() #update the data dictionary as well
+    
+    
+    ''' Various get and set functions'''    
     def updateOrganizer(self, updateTerm):
         self.organizer=updateTerm
     
@@ -174,6 +239,14 @@ class Event:
         
     def addSponsors(self, newSponsor):
         self.sponsors.append(newSponsor)
+        
+    def setID(self, updateTerm):
+        self.ID=updateTerm
+        
+    def getID(self):
+        return self.ID
+    
+    
         
         
 
