72067d72e92fd1f770c95a576a1b7f34d7e6b50e
charlesma4 <charlesma4@gmail.com>
2017-11-04 15:23:44 -0400

    waiting on others heh

--- src/app.py
+++ src/app.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template
 
+import ElasticSearchClient
+ElasticSearchClient.path.append("/..")
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("/../frontEnd/index.html")
 
-# Alan pls check this - I think you don't pass in a database as a parameter, but instead directly reference it within the function e.g. referencing a mongod
-# instance but I'm not sure cause I've never done that stuffs before
-def checkEventID(event, database):
-	if event.id in database:
-		print("Event is already in database. Processing...")
+def checkEventID(event):
+	if event.id in ElasticSearchClient.es:
+		print("Event is already in database.")
 	else
 		database[event.id] = event
-		print("Event is not in database. Adding to database...")
+		print("Event is not in database.")
+
+def createIndex():
+	ElasticSearchClient.createIndex()
 
