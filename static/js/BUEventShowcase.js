8782e0cf172290467ba7b4beab144feb8c053786
ChiragAswani97 <chirag@aswani.net>
2017-11-22 09:17:08 -0500

    Added dynamic tables to index and corrected path to js file

--- static/js/BUEventShowcase.js
+++ static/js/BUEventShowcase.js
@@ -1,7 +1,10 @@
 var BUEventShowcase={
+	logger : null
 }
-BUEventShowcase.documentReady=function(){
+
+BUEventShowcase.documentReady =function() {
 	console.log("BUEventShowcase is ready");
+	BUEventShowcase.buildEventsTable(items);
 }
 BUEventShowcase.searchByID = function(){
 	ids=$('#idField').val()
@@ -32,3 +35,25 @@ BUEventShowcase.createEvent = function(){
 		console.log(response)
 	})
 }
+
+
+
+var items = [
+{ eventName: "Event", organizer: "Chirag", participants : "100000", description : "socks only", registrationRequired : "NO", location : "Boston, MA", address : "IDK", city : "Boston", startTime : "69:00", endTime : "96:00", duration : "69hrs", _id : "69", cost : "69", minCost : "69", maxCost : "69", refundPolicy : "NONE", subOrganizers : "69", sponsors : "69"},
+{ eventName: "NA", organizer: "80", participants : "3", description : "240", registrationRequired : "3", location : "3", address : "3", city : "3", startTime : "3", endTime : "3", duration : "3", _id : "3", cost : "3", minCost : "3", maxCost : "3", refundPolicy : "3", subOrganizers : "3", sponsors : "3"},
+{ eventName: "Apple", organizer: "80", participants : "3", description : "240", registrationRequired : "3", location : "3", address : "3", city : "3", startTime : "3", endTime : "3", duration : "3", _id : "3", cost : "3", minCost : "3", maxCost : "3", refundPolicy : "3", subOrganizers : "3", sponsors : "3"},
+{ eventName: "Apple", organizer: "80", participants : "3", description : "240", registrationRequired : "3", location : "3", address : "3", city : "3", startTime : "3", endTime : "3", duration : "3", _id : "3", cost : "3", minCost : "3", maxCost : "3", refundPolicy : "3", subOrganizers : "3", sponsors : "3"},
+             ];
+BUEventShowcase.buildEventsTable = function(items) {
+	console.log(items);
+	// clear table contents and set up spotify headers
+	resultsTable = $("#resultsTable1");
+	$("#resultsTable1" + " tr:has(td)").remove();
+	resultsTable.find("thead").remove();
+	var header = "<thead><tr><th>eventName</th><th>organizer</th><th>participants</th><th>description</th><th>registrationRequired</th><th>location</th><th>address</th><th>city</th><th>startTime</th><th>endTime</th><th>duration</th><th>_id</th><th>cost</th><th>minCost</th><th>maxCost</th><th>refundPolicy</th><th>subOrganizers</th><th>sponsors</th></tr></thead>";
+	resultsTable.prepend(header);
+	resultsTableBody = resultsTable.find("tbody");
+	for (var i = 0; i < items.length; i++) {
+		resultsTableBody.append($('<tr/>').append($('<td/>').append($("<span/>").text(items[i].eventName))).append($('<td/>').append($("<a/>").text(items[i].organizer))).append($('<td/>').append($("<a/>").text(items[i].participants))).append($('<td/>').append($("<a/>").text(items[i].description))).append($('<td/>').append($("<a/>").text(items[i].registrationRequired))).append($('<td/>').append($("<a/>").text(items[i].location))).append($('<td/>').append($("<a/>").text(items[i].address))).append($('<td/>').append($("<a/>").text(items[i].city))).append($('<td/>').append($("<a/>").text(items[i].startTime))).append($('<td/>').append($("<a/>").text(items[i].endTime))).append($('<td/>').append($("<a/>").text(items[i].duration))).append($('<td/>').append($("<a/>").text(items[i]._id))).append($('<td/>').append($("<a/>").text(items[i].cost))).append($('<td/>').append($("<a/>").text(items[i].minCost))).append($('<td/>').append($("<a/>").text(items[i].maxCost))).append($('<td/>').append($("<a/>").text(items[i].refundPolicy))).append($('<td/>').append($("<a/>").text(items[i].subOrganizers))).append($('<td/>').append(items[i].sponsors)));
+	}
+};
